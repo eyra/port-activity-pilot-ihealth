bf8c12203faee5ce21091114ffb06b84266647f3
Jeroen <jeroen@adaptiveconstant.com>
2023-11-25 14:48:11 +0100

    Added date filter

--- src/framework/processing/py/port/script.py
+++ src/framework/processing/py/port/script.py
@@ -9,8 +9,108 @@ import pandas as pd
 import port.api.props as props
 from port.api.commands import (CommandSystemDonate, CommandSystemExit, CommandUIRender)
 
-import pandas as pd
-import zipfile
+ExtractionResult = namedtuple("ExtractionResult", ["id", "title", "data_frame"])
+
+filter_start_date = datetime(2017, 1, 1)
+
+
+class FileInZipNotFoundError(Exception):
+    """Raised when a specific file is not found within the ZIP archive."""
+
+class EmptyHealthDataError(Exception):
+    """Raised when there are no health data records in the XML."""
+
+class InvalidXMLError(Exception):
+    """Raised when the XML input is invalid or empty."""
+
+
+class HealthDataHandler(xml.sax.ContentHandler):
+    def __init__(self, callback):
+        self.callback = callback
+
+    def startElement(self, tag, attributes):
+        if tag == "Record" and attributes["type"] == "HKQuantityTypeIdentifierStepCount":
+            value = int(attributes["value"])
+            start_date = self.parse_naive_datetime(attributes["startDate"])
+            self.callback(value, start_date)
+            
+    def parse_naive_datetime(self, date_str):
+        dt = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S %z')
+        return dt.replace(tzinfo=None)
+
+class StepCountCallback:
+    def __init__(self):
+        self.start_times = []
+        self.steps = []
+
+    def __call__(self, value, start_date):
+        if start_date < filter_start_date:
+            return
+        self.steps.append(value)
+        self.start_times.append(start_date)
+
+    def to_dataframe(self):
+        return pd.DataFrame({'Start Time': self.start_times, 'Steps': self.steps})
+
+def parse_health_data(file_obj):
+    callback_instance = StepCountCallback()
+
+    parser = xml.sax.make_parser()
+    handler = HealthDataHandler(callback_instance)
+    parser.setContentHandler(handler)
+
+    try:
+        parser.parse(file_obj)
+    except xml.sax.SAXParseException:
+        raise InvalidXMLError("The provided XML input is invalid or empty.")
+
+    df = callback_instance.to_dataframe()
+
+    if df.empty:
+        raise EmptyHealthDataError("No health data records found in the XML input.")
+
+    return df
+
+
+def aggregate_daily_steps(file_obj):
+    # Parse the XML to get initial DataFrame
+    df = parse_health_data(file_obj)
+    
+    # Extract just the date from 'Start Time' column
+    df['Date'] = df['Start Time'].dt.strftime("%Y-%m-%d")
+    
+    # Group by this date and sum the steps
+    daily_steps = df.groupby('Date')['Steps'].sum().reset_index()
+    
+    return daily_steps
+
+
+@contextmanager
+def open_export_zip(zip_path, file_name="apple_health_export/export.xml"):
+    archive = zipfile.ZipFile(zip_path, 'r')
+    if file_name not in archive.namelist():
+        archive.close()
+        raise FileInZipNotFoundError(f"'{file_name}' was not found in the ZIP archive.")
+        
+    try:
+        yield archive.open(file_name)
+    finally:
+        archive.close()
+
+def aggregate_steps_from_zip(zip_path):
+    with open_export_zip(zip_path) as f:
+        return aggregate_daily_steps(f)
+    
+def extract_daily_steps_from_zip(zip_path):
+    step_data = aggregate_steps_from_zip(zip_path)
+    return ExtractionResult(
+    "ihealth_step_counts",
+    props.Translatable(
+        {"en": "Steps", "nl": "Stappen"}
+    ),
+    pd.DataFrame(step_data),
+)
+
 
 
 def process(sessionId):
